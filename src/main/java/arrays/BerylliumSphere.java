2f7f9a6737658d3b49850bff02cf5d2c1129d2c1
razvanmoldovan <r.moldovan@evozon.com>
2014-12-03 13:08:49 +0000

    Third Commit

---
+++ src/main/java/arrays/BerylliumSphere.java
@@ -0,0 +1,13 @@
+package arrays;
+
+/**
+ * Created by razvanmoldovan on 17/11/2014.
+ */
+public class BerylliumSphere {
+    private static long counter;
+    private final long id = counter++;
+
+    public String toString() {
+        return "Sphere " + id;
+    }
+}
